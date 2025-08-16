0da615e7a1e72fb394579572841e22bafe1792cb
jesse-bos-dij <169130613+jesse-bos-dij@users.noreply.github.com>
2025-08-16 13:40:36 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -15,7 +15,7 @@ class TestCase extends Orchestra
         parent::setUp();
 
         Factory::guessFactoryNamesUsing(
-            fn (string $modelName): string => 'DIJ\\Deepgram\\Database\\Factories\\' . class_basename($modelName) . 'Factory'
+            fn (string $modelName): string => 'DIJ\\Deepgram\\Database\\Factories\\'.class_basename($modelName).'Factory'
         );
     }
 
